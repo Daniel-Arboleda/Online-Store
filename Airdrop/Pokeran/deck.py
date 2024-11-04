b7cd462b3c6c7abccab4191cd42b76f383a359e8
Daniel <danielarta1992@gmail.com>
2024-11-03 23:48:49 -0500

    BACKEND: Se actualiza la clase DECK para que pueda ser interpretada en JSON y que el frontend pueda itnerpretar esto en la vista

--- Airdrop/Pokeran/deck.py
+++ Airdrop/Pokeran/deck.py
@@ -13,26 +13,52 @@ class Card:
     def __repr__(self):
         return f"{self.rank} of {self.suit}"
 
+    # def to_dict(self):
+    #     return {"rank": self.rank, "suit": self.suit, "image_filename": self.image_filename}
+
+    def to_dict(self):
+        return {
+            "rank": self.rank,
+            "suit": self.suit,
+            "image_filename": self.image_filename
+        }
+
 # Clase para representar la baraja
 class Deck:
-    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
     ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+    # Inicializa y baraja el mazo automáticamente
+    # def __init__(self):
+    #     self.cards = [Card(rank, suit) for suit in ["Hearts", "Diamonds", "Clubs", "Spades"]
+    #                   for rank in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]]
+    #     random.shuffle(self.cards)
+
+    # def __init__(self):
+    #     self.cards = [{'rank': rank, 'suit': suit} for suit in self.suits for rank in self.ranks]
+    #     random.shuffle(self.cards)  # Baraja las cartas al crear la instancia
 
     # Inicializa y baraja el mazo automáticamente
     def __init__(self):
-        self.cards = [Card(rank, suit) for suit in ["Hearts", "Diamonds", "Clubs", "Spades"]
-                      for rank in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]]
-        random.shuffle(self.cards)
+        self.cards = [Card(suit, rank) for suit in self.suits for rank in self.ranks]
+        self.shuffle()  # Baraja las cartas al crear la instancia
 
     def shuffle(self):
         random.shuffle(self.cards)
+
+    # def deal(self, num_cards):
+    #     if num_cards > len(self.cards):
+    #         raise ValueError("No hay suficientes cartas en la baraja.")
+    #     dealt_cards = self.cards[:num_cards]
+    #     self.cards = self.cards[num_cards:]
+    #     return [{"rank": card.rank, "suit": card.suit, "image_filename": card.image_filename} for card in dealt_cards]
 
     def deal(self, num_cards):
         if num_cards > len(self.cards):
             raise ValueError("No hay suficientes cartas en la baraja.")
         dealt_cards = self.cards[:num_cards]
         self.cards = self.cards[num_cards:]
-        return [{"rank": card.rank, "suit": card.suit, "image_filename": card.image_filename} for card in dealt_cards]
+        return [card.to_dict() for card in dealt_cards]  # Convertir a dict para fácil manejo en el front
 
     def reset(self):
         self.cards = [Card(suit, rank) for suit in self.suits for rank in self.ranks]
@@ -46,3 +72,5 @@ class Deck:
     
     def __repr__(self):
         return f"Deck of {len(self.cards)} cards"
+
+    
